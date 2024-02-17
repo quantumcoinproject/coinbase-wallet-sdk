4289933f1a8ed7d5ae4a61baa0b661f77c761363
Blake Hartin <122081314+blakehartin@users.noreply.github.com>
2024-02-16 18:18:33 -0800

    Update Layout.tsx menu heading

--- apps/testapp/src/components/Layout.tsx
+++ apps/testapp/src/components/Layout.tsx
@@ -1,71 +1,47 @@
-import { CheckIcon, ChevronDownIcon } from '@chakra-ui/icons';
 import {
-  Box,
-  Button,
-  Container,
-  Flex,
-  Heading,
-  Menu,
-  MenuButton,
-  MenuItem,
-  MenuList,
+    Box,
+    Button,
+    Container,
+    Flex,
+    Heading,
 } from '@chakra-ui/react';
 
 import { sdkVersions, useCBWSDK } from '../context/CBWSDKProvider';
 
 type LayoutProps = {
-  children: React.ReactNode;
+    children: React.ReactNode;
 };
 
 export const WIDTH_2XL = '1536px';
 
 export function Layout({ children }: LayoutProps) {
-  const { sdk, sdkVersion, setSDKVersion } = useCBWSDK();
+    const { sdk, sdkVersion, setSDKVersion } = useCBWSDK();
 
-  const handleClockDocs = () => {
-    window.open('https://cbdev.io/walletstart', '_blank');
-  };
+    const handleClockDocs = () => {
+        window.open('https://dpdocs.org/getting-coins-for-tokens-cli-wallet.html', '_blank');
+    };
 
-  const handleDisconnect = () => {
-    if (sdk) {
-      sdk.disconnect();
-    }
-  };
+    const handleDisconnect = () => {
+        if (sdk) {
+            sdk.disconnect();
+        }
+    };
 
-  return (
-    <Box minH="100vh" bg="blackAlpha.100">
-      <Box as="header" shadow="lg" py={6} bg="blackAlpha.900" color="whiteAlpha.900">
-        <Container maxW={WIDTH_2XL}>
-          <Flex justifyContent="space-between" alignItems="center">
-            <Heading>Coinbase Wallet SDK - Playground</Heading>
-            <Flex justifyContent="space-between" alignItems="center" gap={4}>
-              <Button onClick={handleClockDocs}>Docs</Button>
-              <Menu>
-                <MenuButton as={Button} rightIcon={<ChevronDownIcon />}>
-                  {`SDK: ${sdkVersion}`}
-                </MenuButton>
-                <MenuList>
-                  {sdkVersions.map((version) => (
-                    <MenuItem
-                      color={'MenuText'}
-                      key={version}
-                      icon={version === sdkVersion ? <CheckIcon /> : null}
-                      onClick={() => setSDKVersion(version)}
-                    >
-                      {version}
-                    </MenuItem>
-                  ))}
-                </MenuList>
-              </Menu>
-              {/* TODO: There is an issue where `this` is undefined within the sdk instance. */}
-              <Button onClick={handleDisconnect}>Disconnect</Button>
+    return (
+        <Box minH="100vh" bg="blackAlpha.100">
+            <Box as="header" shadow="lg" py={6} bg="blackAlpha.900" color="whiteAlpha.900">
+                <Container maxW={WIDTH_2XL}>
+                    <Flex justifyContent="space-between" alignItems="center">
+                        <Heading>Doge Protocol Coins For Tokens</Heading>
+                        <Flex justifyContent="space-between" alignItems="center" gap={4}>
+                            <Button onClick={handleClockDocs}>Help</Button>
+                        </Flex>
+                    </Flex>
+                </Container>
+            </Box>
+            <Flex flex={1} as="main" mt={6}>
+                {children}
             </Flex>
-          </Flex>
-        </Container>
-      </Box>
-      <Flex flex={1} as="main" mt={6}>
-        {children}
-      </Flex>
-    </Box>
-  );
+        </Box>
+    );
 }
