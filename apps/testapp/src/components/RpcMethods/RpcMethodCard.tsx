05d04c94449177e2bea56058c631debd5b088a06
Blake Hartin <122081314+blakehartin@users.noreply.github.com>
2024-02-17 14:18:16 -0800

    Update RpcMethodCard.tsx react sdk

--- apps/testapp/src/components/RpcMethods/RpcMethodCard.tsx
+++ apps/testapp/src/components/RpcMethods/RpcMethodCard.tsx
@@ -1,223 +1,228 @@
 import {
-  Accordion,
-  AccordionButton,
-  AccordionIcon,
-  AccordionItem,
-  AccordionPanel,
-  Button,
-  Card,
-  CardBody,
-  Code,
-  Flex,
-  FormControl,
-  FormErrorMessage,
-  Heading,
-  Input,
-  InputGroup,
-  InputLeftAddon,
-  Radio,
-  RadioGroup,
-  Stack,
-  VStack,
+    Accordion,
+    AccordionButton,
+    AccordionIcon,
+    AccordionItem,
+    AccordionPanel,
+    Button,
+    Card,
+    CardBody,
+    Code,
+    Flex,
+    FormControl,
+    FormErrorMessage,
+    Heading,
+    Input,
+    InputGroup,
+    InputLeftAddon,
+    Radio,
+    RadioGroup,
+    Stack,
+    VStack,
 } from '@chakra-ui/react';
 import React, { useCallback } from 'react';
 import { useForm } from 'react-hook-form';
 
 import { useCBWSDK } from '../../context/CBWSDKProvider';
 import { ADDR_TO_FILL } from './shortcut/const';
-import { MetaMaskSDK, SDKProvider } from '@metamask/sdk';
+import { useSDK } from "@metamask/sdk-react";
 
 export function RpcMethodCard({ connected, format, method, params, shortcuts }) {
-  const [response, setResponse] = React.useState<any | null>(null);
-  const [error, setError] = React.useState<Record<string, unknown> | string | number | null>(null);
-  const { provider } = useCBWSDK();
-  const [sdk, setSDK] = React.useState<MetaMaskSDK>();
-  
-  const {
-    handleSubmit,
-    register,
-    formState: { errors },
-  } = useForm();
+    const [response, setResponse] = React.useState<any | null>(null);
+    const [error, setError] = React.useState<Record<string, unknown> | string | number | null>(null);
+    const { provider } = useCBWSDK();
+    const { sdk } = useSDK();
+    //const [signedMessage, setSignedMessage] = React.useState("");
 
-const connectAndSignMetaMask = async (message: string) => {
-    try {
-        console.log(message);
-        const signResult = await sdk?.connectAndSign({
-            msg: message
-        });
-        setResponse(signResult);
-    } catch (err) {
-        setResponse(response);
-    }
-};
+    const {
+        handleSubmit,
+        register,
+        formState: { errors },
+    } = useForm();
 
-const connectAndSignCoinbase = async (data: Record<string, string>) => {
-    setError(null);
-    setResponse(null);
-    if (!provider) return;
-    let values = data;
-    if (format) {
-        // fill active address to the request
-        const addresses = await provider.request({ method: 'eth_accounts' });
-        for (const key in data) {
-            if (Object.prototype.hasOwnProperty.call(data, key)) {
-                if (data[key] === ADDR_TO_FILL) {
-                    data[key] = addresses[0];
+    const connectAndSignMetaMask = async (message: string) => {
+        try {
+            console.log(message);
+            const signature = await sdk.connectAndSign({ msg: message });
+            console.log(signature);
+            setResponse(signature);
+        } catch (err) {
+            if (err instanceof Error) {
+                setError(err.message);
+            } else {
+                const { code, message } = err;
+                setError({ code, message });
+            }
+        }
+    };
+
+    const connectAndSignCoinbase = async (data: Record<string, string>) => {
+        setError(null);
+        setResponse(null);
+        if (!provider) return;
+        let values = data;
+        if (format) {
+            // fill active address to the request
+            const addresses = await provider.request({ method: 'eth_accounts' });
+            for (const key in data) {
+                if (Object.prototype.hasOwnProperty.call(data, key)) {
+                    if (data[key] === ADDR_TO_FILL) {
+                        data[key] = addresses[0];
+                    }
                 }
             }
+            values = format(data);
         }
-        values = format(data);
-    }
-    try {
-        // connection required
-        if (!connected) {
-            await provider.enable();
-        }
-        const response = await provider.request({
-            method,
-            params: values,
-        });
-        setResponse(response);
-    } catch (err) {
-        if (err instanceof Error) {
-            setError(err.message);
-        } else {
-            const { code, message } = err;
-            setError({ code, message });
-        }
-    }
-};
-
-  const submit = useCallback(
-      async (data: Record<string, string>) => {
-        for (const key in data) {
-              console.log(key + "=" + data[key]);
-        }
-        const walletType = data["walletType"];
-        if (walletType === 'coinbase') {
-            connectAndSignCoinbase(data);
-            return;
-        } else if (walletType === 'metamask') {
-            connectAndSignMetaMask(data["message"]);
-            return;
-        } else {
-            setError('Select a wallet type');
-            return;
-        }
-
-     /* setError(null);
-      setResponse(null);
-      if (!provider) return;
-      let values = data;
-      if (format) {
-        // fill active address to the request
-        const addresses = await provider.request({ method: 'eth_accounts' });
-        for (const key in data) {
-          if (Object.prototype.hasOwnProperty.call(data, key)) {
-            if (data[key] === ADDR_TO_FILL) {
-              data[key] = addresses[0];
+        try {
+            // connection required
+            if (!connected) {
+                await provider.enable();
             }
-          }
+            const response = await provider.request({
+                method,
+                params: values,
+            });
+            setResponse(response);
+        } catch (err) {
+            if (err instanceof Error) {
+                setError(err.message);
+            } else {
+                const { code, message } = err;
+                setError({ code, message });
+            }
         }
-        values = format(data);
-      }
-      try {
-        // connection required
-        if (!connected) {
-          await provider.enable();
-        }
-        const response = await provider.request({
-          method,
-          params: values,
-        });
-        setResponse(response);
-      } catch (err) {
-        if (err instanceof Error) {
-          setError(err.message);
-        } else {
-          const { code, message } = err;
-          setError({ code, message });
-        }
-      }*/
-    },
-    [provider]
-  );
+    };
 
-  return (
-    <Card shadow="lg" as="form" onSubmit={handleSubmit(submit)} size='lg'>
-      <CardBody>
-        {params?.length > 0 && (
-          <>
-            <Accordion allowMultiple mt={4} defaultIndex={shortcuts ? [1] : [0]}  index={[0]}>
-              <AccordionItem>
-                <AccordionButton>
-                  <Heading as="h3" size="sm" marginY={2} flex="1" textAlign="left">
-                    Signing Details
-                  </Heading>
-                  <AccordionIcon />
-                </AccordionButton>
-                <AccordionPanel pb={4}>
-                    <VStack spacing={2} mt={2}>
-                        <FormControl key="walletType" isRequired={true}>
-                            <RadioGroup name="walletType" >
-                                <Stack direction='row'>
-                                    <Radio value='coinbase' {...register("walletType", {})}>Coinbase Wallet</Radio>
-                                    <Radio value='metamask' {...register("walletType", {})}>MetaMask</Radio>
-                                </Stack>
-                            </RadioGroup>
-                        </FormControl>
-                    {params.map((param) => {
-                      const err = errors[param.key];
-                      return (
-                          <FormControl key={param.key} isInvalid={!!err} isRequired={param.required}>
-                          <InputGroup size="lg">
-                            <InputLeftAddon>{param.key}</InputLeftAddon>
-                            <Input size='lg'
-                              {...register(param.key, {
-                                required: param.required ? `${param.key} required` : false,
-                              })}
-                            />
-                          </InputGroup>
-                          <FormErrorMessage>{err?.message as string}</FormErrorMessage>
-                        </FormControl>
-                      );
-                    })}
-                  </VStack>
-                </AccordionPanel>
-              </AccordionItem>
-            </Accordion>
-          </>
-        )}
-         <Flex align="center" justify="space-between">
-          <Button type="submit" mt={4}>
-            Submit
-          </Button>
-        </Flex>
-        {response && (
-          <VStack mt={4}>
-            <Heading as="h4" size="sm" marginY={2} flex="1" textAlign="left">
-              Signature
-            </Heading>
-            <Code as="pre" p={4} wordBreak="break-word" whiteSpace="pre-wrap" w="100%"  colorScheme="green">
-              {JSON.stringify(response, null, 2)}
-            </Code>
-          </VStack>
-        )}
-        {error && (
-          <VStack mt={4}>
-            <Code
-              as="pre"
-              colorScheme="red"
-              p={4}
-              wordBreak="break-word"
-              whiteSpace="pre-wrap"
-              w="100%"
-            >
-              {JSON.stringify(error, null, 2)}
-            </Code>
-          </VStack>
-        )}
-      </CardBody>
-    </Card>
-  );
+    const submit = useCallback(
+        async (data: Record<string, string>) => {
+            for (const key in data) {
+                console.log(key + "=" + data[key]);
+            }
+            const walletType = data["walletType"];
+            if (walletType === 'coinbase') {
+                connectAndSignCoinbase(data);
+                return;
+            } else if (walletType === 'metamask') {
+                connectAndSignMetaMask(data["message"]);
+                return;
+            } else {
+                setError('Select a wallet type');
+                return;
+            }
+
+            /* setError(null);
+             setResponse(null);
+             if (!provider) return;
+             let values = data;
+             if (format) {
+               // fill active address to the request
+               const addresses = await provider.request({ method: 'eth_accounts' });
+               for (const key in data) {
+                 if (Object.prototype.hasOwnProperty.call(data, key)) {
+                   if (data[key] === ADDR_TO_FILL) {
+                     data[key] = addresses[0];
+                   }
+                 }
+               }
+               values = format(data);
+             }
+             try {
+               // connection required
+               if (!connected) {
+                 await provider.enable();
+               }
+               const response = await provider.request({
+                 method,
+                 params: values,
+               });
+               setResponse(response);
+             } catch (err) {
+               if (err instanceof Error) {
+                 setError(err.message);
+               } else {
+                 const { code, message } = err;
+                 setError({ code, message });
+               }
+             }*/
+        },
+        [provider]
+    );
+
+    return (
+        <Card shadow="lg" as="form" onSubmit={handleSubmit(submit)} size='lg'>
+            <CardBody>
+                {params?.length > 0 && (
+                    <>
+                        <Accordion allowMultiple mt={4} defaultIndex={shortcuts ? [1] : [0]} index={[0]}>
+                            <AccordionItem>
+                                <AccordionButton>
+                                    <Heading as="h3" size="sm" marginY={2} flex="1" textAlign="left">
+                                        Signing Details
+                                    </Heading>
+                                    <AccordionIcon />
+                                </AccordionButton>
+                                <AccordionPanel pb={4}>
+                                    <VStack spacing={2} mt={2}>
+                                        <FormControl key="walletType" isRequired={true}>
+                                            <RadioGroup name="walletType" >
+                                                <Stack direction='row'>
+                                                    <Radio value='coinbase' {...register("walletType", {})}>Coinbase Wallet</Radio>
+                                                    <Radio value='metamask' {...register("walletType", {})}>MetaMask</Radio>
+                                                </Stack>
+                                            </RadioGroup>
+                                        </FormControl>
+                                        {params.map((param) => {
+                                            const err = errors[param.key];
+                                            return (
+                                                <FormControl key={param.key} isInvalid={!!err} isRequired={param.required}>
+                                                    <InputGroup size="lg">
+                                                        <InputLeftAddon>{param.key}</InputLeftAddon>
+                                                        <Input size='lg'
+                                                            {...register(param.key, {
+                                                                required: param.required ? `${param.key} required` : false,
+                                                            })}
+                                                        />
+                                                    </InputGroup>
+                                                    <FormErrorMessage>{err?.message as string}</FormErrorMessage>
+                                                </FormControl>
+                                            );
+                                        })}
+                                    </VStack>
+                                </AccordionPanel>
+                            </AccordionItem>
+                        </Accordion>
+                    </>
+                )}
+                <Flex align="center" justify="space-between">
+                    <Button type="submit" mt={4}>
+                        Submit
+                    </Button>
+                </Flex>
+                {response && (
+                    <VStack mt={4}>
+                        <Heading as="h4" size="sm" marginY={2} flex="1" textAlign="left">
+                            Signature
+                        </Heading>
+                        <Code as="pre" p={4} wordBreak="break-word" whiteSpace="pre-wrap" w="100%" colorScheme="green">
+                            {JSON.stringify(response, null, 2)}
+                        </Code>
+                    </VStack>
+                )}
+                {error && (
+                    <VStack mt={4}>
+                        <Code
+                            as="pre"
+                            colorScheme="red"
+                            p={4}
+                            wordBreak="break-word"
+                            whiteSpace="pre-wrap"
+                            w="100%"
+                        >
+                            {JSON.stringify(error, null, 2)}
+                        </Code>
+                    </VStack>
+                )}
+            </CardBody>
+        </Card>
+    );
 }
